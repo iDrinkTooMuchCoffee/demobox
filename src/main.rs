83b98ad24d636a9e0013421374d20398dd2cb1b4
Anthony <famedanthony@hotmail.com>
2018-05-28 14:30:05 -0400

    basic file opening

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
 use std::fs;
+use std::env;
+use std::fs::File;
 use std::io::prelude::*;
-
-const FOLDER_PATH: &'static str = "C:/Users/Anthony/Desktop/Demos";
+use std::fs::OpenOptions;
 
 fn main() {
-    list_demos(FOLDER_PATH.to_string());
+    list_demos(read_cfg().to_string());
+}
+
+// Reads from the user cfg file.
+fn read_cfg() -> &'static str {
+    let cfg = File::open("config.ini");
+    if cfg.is_err() {
+        let cfg = File::create("config.ini");
+    }
+
+    // TODO
+    // TEMPORARY
+    let demopath = "Demos";
+    return demopath;
 }
 
 // Lists all demos found in the path.
 fn list_demos(path: String) { 
-    let dir = fs::read_dir(path).unwrap();
-    let pathlen: usize = FOLDER_PATH.len();
+    let dir = fs::read_dir(&path);
+    let pathlen: usize = path.len();
+    let dir = match dir {
+        Ok(dct) => dct,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
+
     for file in dir {
-        let slice = &file.unwrap().path().display().to_string()[pathlen..];
+        let slice = &file.unwrap().path().display().to_string()[pathlen..]; // Substring to just the filename.
         println!("{}", slice)
     }
 }
